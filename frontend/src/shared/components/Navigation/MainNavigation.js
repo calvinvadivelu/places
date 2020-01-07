b4dede781a29bb9214b016325311500fe3230da4
cvadivelu <50369619+cvadivelu@users.noreply.github.com>
2020-01-07 17:17:58 -0500

    creating /places as well as associated navigation

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -19,15 +19,13 @@ const MainNavigation = () => {
     return (
         <>
             {drawerIsOpen && 
-                <>
                 <Backdrop onClick={closeDrawer}/>
-                <SideDrawer>
-                    <nav className="main-navigation__drawer-nav">
-                        <NavLinks />
-                    </nav>
-                </SideDrawer>
-                </>
             }
+            <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
+                <nav className="main-navigation__drawer-nav">
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
             <MainHeader>
                 <button className='main-navigation__menu-btn' onClick={openDrawer}>
                     <span></span>
--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -6,7 +6,7 @@ const NavLinks = props => {
     return (
         <ul className="nav-links">
             <li>
-                <NavLink to={'/'}>ALL USERS</NavLink>
+                <NavLink exact to={'/'}>ALL USERS</NavLink>
             </li>
             <li>
                 <NavLink to={'/u1/places'}>MY PLACES</NavLink>
